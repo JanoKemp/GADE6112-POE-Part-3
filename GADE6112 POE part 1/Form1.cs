b5f4f3cb1416fd929af7c8db5aef12f2f7c79ae9
adaml <adaml@DESKTOP-HDDSHVT>
2022-09-03 11:39:17 +0200

    Add project files.

---
+++ GADE6112 POE part 1/Form1.cs
@@ -0,0 +1,16 @@
+
+namespace GADE6112_POE_part_1
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
