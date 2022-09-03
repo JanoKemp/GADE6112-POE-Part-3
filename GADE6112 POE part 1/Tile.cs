0b2df2cddc56599596df080bfad5695daf0ad791
AmLisBack <ST10069132@vgconnect.edu.za>
2022-09-03 13:34:45 +0200

    Added Tile class and variables

---
+++ GADE6112 POE part 1/Tile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6112_POE_part_1
+{
+    internal class Tile
+    {
+        protected int x, y;
+        public enum TileType
+        {
+            Hero = 1 ,
+            Enemy = 2,
+            Gold = 3, 
+            Weapon =4
+        }
+    }
+}
