b7346eaa22b6b66c28b5517c25e9fa92db6b1945
JanoKemp <kempjano@gmail.com>
2022-09-03 14:33:26 +0200

    added Tile consturctor

--- GADE6112 POE part 1/Tile.cs
+++ GADE6112 POE part 1/Tile.cs
@@ -11,10 +11,17 @@ namespace GADE6112_POE_part_1
         protected int x, y;
         public enum TileType
         {
-            Hero = 1 ,
-            Enemy = 2,
-            Gold = 3, 
-            Weapon =4
+            Hero  ,
+            Enemy ,
+            Gold , 
+            Weapon
+        }
+        
+        public Tile(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            
         }
     }
 }
