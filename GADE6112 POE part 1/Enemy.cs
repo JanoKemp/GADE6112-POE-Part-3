b4de8513e0a33ca1e240d717c665d5b7ea50c6e4
JanoKemp <kempjano@gmail.com>
2022-09-05 11:16:24 +0200

    added enemy class and some contents

---
+++ GADE6112 POE part 1/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6112_POE_part_1
+{
+    internal abstract class Enemy: Character
+    {
+        protected Random rnd = new Random();  
+
+        public Enemy(int hP, int maxHP, int damage)
+        {
+
+        }
+    }
+}
