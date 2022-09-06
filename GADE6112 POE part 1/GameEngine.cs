29a69a915d1248483226b89cd09679d95c639e85
AmLisBack <ST10069132@vgconnect.edu.za>
2022-09-06 14:37:57 +0200

    Added Game Engine Class

---
+++ GADE6112 POE part 1/GameEngine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6112_POE_part_1
+{
+    
+    internal class GameEngine 
+    {
+       public GameEngine()
+        {
+            Map map = new Map();
+            
+        }
+        public bool MovePlayer(Character.Movement direction)
+        {
+            //This updates the move is the movement is valid via button presses
+            return false;
+        }
+    }
+}
