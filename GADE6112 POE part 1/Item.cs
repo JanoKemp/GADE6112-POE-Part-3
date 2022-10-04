7d3c32ddc78e3130aaa1d880a11ece2515793576
JanoKemp <kempjano@gmail.com>
2022-10-04 11:21:26 +0200

    added item class

---
+++ GADE6112 POE part 1/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6112_POE_part_1
+{
+    abstract internal class Item
+    {
+        public Item(int x, int y)
+        {
+
+        } 
+    }
+}
