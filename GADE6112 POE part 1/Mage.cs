be6b9fd76cc39d2358a0ef708977f8a170850075
JanoKemp <kempjano@gmail.com>
2022-10-04 12:08:46 +0200

    add mage

---
+++ GADE6112 POE part 1/Mage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6112_POE_part_1
+{
+    internal class Mage: Enemy
+    {
+        public Mage(int x, int y)
+        {
+            this.x = getX();// gets X and Y
+            this.y = getY();
+
+            this.hp = 5;
+            this.damage = 5;
+        }
+
+        public override Movement ReturnMove(Movement direction = Movement.noMovement)
+        {
+            return direction;
+        }
+    }
+}
