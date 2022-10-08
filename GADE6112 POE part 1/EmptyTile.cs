894a5b6ae28b11e24d1b92f51c07867c0c6a5539
AmLisBack <ST10069132@vgconnect.edu.za>
2022-10-08 12:09:33 +0200

    Modified Map and EmptyTile and Obstacle to incorparate making the map

--- GADE6112 POE part 1/EmptyTile.cs
+++ GADE6112 POE part 1/EmptyTile.cs
@@ -8,9 +8,9 @@ namespace GADE6112_POE_part_1
 {
     internal class EmptyTile : Tile
     {
-        public EmptyTile(int x , int y,TileType type): base() // Used for creating Empty tiles across the Map
+        public EmptyTile(): base() // Used for creating Empty tiles across the Map
         {
-            type = TileType.Clear;
+           
         }
     }
 }
--- GADE6112 POE part 1/Obstacle.cs
+++ GADE6112 POE part 1/Obstacle.cs
@@ -8,9 +8,9 @@ namespace GADE6112_POE_part_1
 {
     internal class Obstacle : Tile
     {
-        public Obstacle (int x , int y, TileType type): base ()
+        public Obstacle (): base ()
             {
-                type = TileType.Barrier;
+             
             }
     }
    
